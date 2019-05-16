ee8fda5575d14d2e32bf9171e63fa9c80a2ed386
AntonStriga <glau_76@mail.ru>
2019-05-16 15:45:02 +0300

    Lesson 0. Test 1
    Результат Теста 1 к Уроку 0. Введение в Java и JUnit

---
+++ src/MainClassTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass
+{
+    @Test
+    public void testGetLocalNumber()
+    {
+        Assert.assertEquals("Returned value is not 14", 14, this.getLocalNumber());
+    }
+}
